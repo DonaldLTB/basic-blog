e05cb0294bf0ffc7c4353e4e5935a03646fe085e
Lawrence Kroll <larry43.lk@gmail.com>
2021-05-28 17:36:34 +0900

    added pg search and search box

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,10 @@
 class Article < ApplicationRecord
   belongs_to :user
   is_impressionable
+  include PgSearch::Model
+  pg_search_scope :search_by_title,
+    against: [ :title ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
