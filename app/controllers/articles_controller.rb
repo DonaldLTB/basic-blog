233bd02c56c6921930f845e1a784b12835674771
Lawrence Kroll <larry43.lk@gmail.com>
2021-05-28 16:30:27 +0900

    new create working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,27 @@
 class ArticlesController < ApplicationController
     skip_before_action :authenticate_user!, only: [:index, :show]
-    before_action :find_article, only: [:new, :show, :edit, :update, :destroy]
+    before_action :find_article, only: [:show]
 
     def index
       @articles = Article.all
     end
 
     def show; end
+
+    def new; 
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        @article.user = current_user
+        @article.public = false if @article.public.nil?
+        if @article.save
+            redirect_to articles_path
+        else
+            redirect_to articles_path, notice: "Please try again"
+        end
+    end
 
     def update
     end
@@ -17,5 +32,9 @@ class ArticlesController < ApplicationController
     def find_article
         @article = Article.find(params[:id])
     end
+
+    def article_params
+        params.require(:article).permit(:title, :content, :publish_date, :public)
+      end
 end
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_28_062803) do
+ActiveRecord::Schema.define(version: 2021_05_28_071921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_05_28_062803) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "content"
-    t.boolean "public", default: false
+    t.boolean "public"
     t.date "publish_date"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
