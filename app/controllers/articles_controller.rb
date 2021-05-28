edfcc2b27f4e7b2729c3ac4ce80d52b9c4a1656e
Lawrence Kroll <larry43.lk@gmail.com>
2021-05-28 15:55:24 +0900

    show added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,21 @@
 class ArticlesController < ApplicationController
-    skip_before_action :authenticate_user!, only: :index
-      
+    skip_before_action :authenticate_user!, only: [:index, :show]
+    before_action :find_article, only: [:new, :show, :edit, :update, :destroy]
+
     def index
       @articles = Article.all
     end
-  end
+
+    def show; end
+
+    def update
+    end
+
+    def edit; end
+    private
+
+    def find_article
+        @article = Article.find(params[:id])
+    end
+end
   
